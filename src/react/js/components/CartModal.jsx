366eef59cb5a57092083690840d0ce4a112f306f
joshjones49 <joshjones200285@gmail.com>
2024-01-02 16:45:29 -0600

    `

--- src/react/js/components/CartModal.jsx
+++ src/react/js/components/CartModal.jsx
@@ -4,11 +4,25 @@ const CartModal = () => {
   return (
     <div id ='cart-modal'>
 
-        <div id='shopping-cart'>
-            <div id='shopping-cart-header' >
-                <h1 id='shopping-cart-h1' >
+        <div id='cart'>
+            <div id='cart-header' >
+                <h1 id='cart-h1' >
                     Shopping Cart
                 </h1>
+                
+                {/* <div id='cart-holder' >
+
+                    <div id='cart-terms' >
+
+                    </div>
+                    <div id='cart-total' >
+
+                    </div>
+                    <div id='cart-promo' >
+
+                    </div>
+
+                </div> */}
             </div>
         </div>
       
