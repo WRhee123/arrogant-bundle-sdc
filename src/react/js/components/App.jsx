9e25e03f235639bc56ffb4564004ea589c723947
kiel buchergallimore <kielbucher@gmail.com>
2024-01-02 16:44:25 -0600

    footer branch

--- src/react/js/components/App.jsx
+++ src/react/js/components/App.jsx
@@ -1,9 +1,12 @@
 import Banner from "./Banner.jsx"
+import Footer from "./Footer.jsx"
+
 
 const App = () => {
     return (
         <>
             <Banner />
+            <Footer />
         </>
     )
 }
---
+++ src/react/js/components/Footer.jsx
@@ -0,0 +1,47 @@
+const Footer = () => {
+
+    return (
+        <div id="footer">
+            <div id="invite-friend">
+                <p>Invite a friend</p>
+            </div>
+
+            <div id="about">
+                <h2>ABOUT</h2>
+                <p>Blog</p>
+                <p>Developer</p>
+                <p>Support</p>
+            </div>
+
+            <div id="trending-games">
+                <h2>TRENDING GAMES</h2>
+                <a href="https://www.humblebundle.com/store/beamngdrive">
+                    <p>BeamNG.drive</p>
+                </a>
+                <p>Marvel's Midnight Suns Season Pass</p>
+                <p>BONEWORKS</p>
+                <p>DAVE THE DIVER</p>
+                <p>Farthest Frontier</p>
+                <p>The Callisto Protocol - Deluxe Edition</p>
+                <p>Destiny 2: Lightfall + Annual Pass</p>
+                <p>ELDEN RING</p>
+            </div>
+
+            <div id="language-social">
+                <p>select language</p>
+                <button>F</button>
+                <button>Twt</button>
+                <button>Inst</button>
+                <button>Blog</button>
+
+            </div>
+
+            <div id="copyright">
+                <p>© 2011-2022 Humble Bundle, Inc. a Ziff Davis company. All Rights Reserved. Humble Bundle and Humble Store are registered trademarks of Humble Bundle, Inc. and may not be used by third parties without express written permission.</p>
+            </div>
+        </div>
+    )
+
+}
+
+export default Footer
