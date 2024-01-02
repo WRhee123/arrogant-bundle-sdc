b2723a8439a644beb4e6c5642509b3f09010308b
Thiem (Tim) Huynh <thiem.huynh0@gmail.com>
2024-01-02 17:42:34 -0500

    Feature/skeleton1 (#7)
    * got page loading live changes. app now serves both static frontend and proxy redirect for api calls.
    
    * added fileURLToPath to enable crossplatform pathing
    
    * added tailwindcdn, title change
    
    * setup humble color palette
    
    * i guess we're not using tailwind? discuss with group
    
    * removed tailwind for now
    
    * adjusted from draft pull. remove redundant fileURLToPath import
    
    * making skeleton
    
    * skeleton update half way done, shifted nav and banner outside of body.
    
    * added color to h1
    
    * skeleton update for footer and popular carousel
    
    * moved navbar to it's own component
    
    * added footer and added nav and banner into their own components
    
    * skeleton complete

---
+++ src/react/js/components/AppContent/Hero.jsx
@@ -0,0 +1,21 @@
+const Hero = () => {
+
+    return (
+    <>
+        <div className="hero ctn">
+            <div className="hero-content">
+                <h1>hero</h1>
+                <div className='product-hero ctn'>
+                    <h2>Super Mario RPG</h2>
+                </div>
+                <div className='main-carousel ctn'>
+                    <h1>main-carousel</h1>
+                </div>
+            </div>
+        </div>
+    </>
+
+    )
+}
+
+export default Hero
---
+++ src/react/js/components/AppContent/SystemReq.jsx
@@ -0,0 +1,19 @@
+const SystemReq = () => {
+
+    return (
+    <>
+        <div className='system-req ctn'>
+            <h1>system-req</h1>
+            <div>
+            SYSTEM REQUIREMENTS
+            Platform: Nintendo Switch
+
+            Supported Languages: Dutch, English, French, German, Italian, Japanese, Korean, Simplified Chinese, Spanish, and Traditional Chinese
+            </div>
+        </div>
+    </>
+
+    )
+}
+
+export default SystemReq
