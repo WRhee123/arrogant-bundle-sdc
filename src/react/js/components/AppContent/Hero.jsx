435844b79f451c5d0457156e1933e7bfd17d6505
Thiem Huynh <thiem.huynh0@gmail.com>
2024-01-03 13:17:01 -0500

    added hero section images

--- src/react/js/components/AppContent/Hero.jsx
+++ src/react/js/components/AppContent/Hero.jsx
@@ -7,29 +7,43 @@ const Hero = ({productData}) => {
         price, 
         humble_choice_savings: savings,
         gameplay_image_links: images,
+        platforms: platImg,
     } = productData;
 
-    console.log({images})
+    // console.log({images})
 
     return (
     <>
-        <div id='hero' className="ctn">
+        <div id='hero' className="">
             <div id="hero-content">
                 <div id='product-hero' className='ctn'>
                     <div className="product-title">
                         <h2>{title}</h2>
                     </div>
-                    <div className="main-carousel-img">
-                        <img src={`https://arrogant-bundle.onrender.com/${images[0]}`}/>
+                    <div className="main-img">
+                        <img src={`https://arrogant-bundle.onrender.com/${images[0]}`} className="main-carousel-img"/>
                     </div>
-                    <div className="shop-section">
-                        <img src={`https://arrogant-bundle.onrender.com/${thumbs}`} alt={title} />
+                    <div id='mini-product'>
+                        <div className="thumbnail">
+                            <img src={`https://arrogant-bundle.onrender.com/${thumbs}`} alt={title} />
+                        </div>
+                        <div className="itemlogo">
+                            <img src={`https://arrogant-bundle.onrender.com${platImg[0].image_link}`} alt="" />
+                        </div>
+                        <div className="price">
+                            {price}
+                        </div>
+                        <div className="savings">
+                            {`SAVE UP TO $${(savings).toFixed(2)} MORE WITH HUMBLE CHOICE`}
+                        </div>
+                        <button>ADD TO CART</button>
+                        <button>ADD TO WISHLIST</button>
                     </div>
                 </div>
                 <div id='hero-carousel' className='ctn'>
                     {/* <HeroCarousel/> */}
                 </div>
-            </div>
+            </div> {/* end hero-content */}
         </div>
     </>
 
