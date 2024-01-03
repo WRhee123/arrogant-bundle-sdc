d42de792b58fe3c63521067c837dee0805822e4c
Thiem Huynh <thiem.huynh0@gmail.com>
2024-01-03 17:44:03 -0500

    hero section complete

--- src/react/js/components/AppContent/Hero.jsx
+++ src/react/js/components/AppContent/Hero.jsx
@@ -1,4 +1,6 @@
-import HeroCarousel from "./HeroCarousel";
+import { FaShoppingCart } from "react-icons/fa";
+import { TbStarFilled } from "react-icons/tb";
+import HeroCarousel from './HeroCarousel';
 
 const Hero = ({productData}) => {
 
@@ -14,35 +16,39 @@ const Hero = ({productData}) => {
 
     return (
     <>
-        <div id='hero' className="">
+        <div id='hero'>
             <div id="hero-content">
-                <div id='product-hero' className='ctn'>
-                    <div className="product-title">
+                <div className="product-title">
                         <h2>{title}</h2>
-                    </div>
+                </div>
+                <div id='product-hero'>
                     <div className="main-img">
                         <img src={`https://arrogant-bundle.onrender.com/${images[0]}`} className="main-carousel-img"/>
                     </div>
                     <div id='mini-product'>
-                        <div className="thumbnail">
-                            <img src={`https://arrogant-bundle.onrender.com/${thumbs}`} alt={title} />
+                        <div className='thumbnail-ctn'>
+                            <img src={`https://arrogant-bundle.onrender.com/${thumbs}`} alt={title} className="thumbnail" />
                         </div>
-                        <div className="itemlogo">
-                            <img src={`https://arrogant-bundle.onrender.com${platImg[0].image_link}`} alt="" />
+                        
+                        <div className="product-showcase">
+                            <div className="itemlogo-ctn">
+                                <img src={`https://arrogant-bundle.onrender.com${platImg[0].image_link}`} className="itemlogo" />
+                            </div>
+                            <div className="price">
+                                ${price}
+                            </div>
+                            <div className="savings">
+                                SAVE UP TO <span className="humble-green">${(savings).toFixed(2)} </span>
+                                MORE WITH <span className="humble-gold">HUMBLE CHOICE</span>
+                            </div>
+                            <div id='checkoutBtn'><span><FaShoppingCart/> </span>CHECKOUT</div>
+                            <div id='wishListBtn'><span><TbStarFilled/> </span>ADD TO WISHLIST</div>
                         </div>
-                        <div className="price">
-                            {price}
-                        </div>
-                        <div className="savings">
-                            SAVE UP TO <span className="humble-green">${(savings).toFixed(2)} </span>
-                            MORE WITH <span className="humble-gold">HUMBLE CHOICE</span>
-                        </div>
-                        <button id='addToCart'>ADD TO CART</button>
-                        <button id='addWishList'>ADD TO WISHLIST</button>
+                        
                     </div>
                 </div>
                 <div id='hero-carousel' className='ctn'>
-                    {/* <HeroCarousel/> */}
+                    <HeroCarousel/>
                 </div>
             </div> {/* end hero-content */}
         </div>
--- src/react/js/components/AppContent/HeroCarousel.jsx
+++ src/react/js/components/AppContent/HeroCarousel.jsx
@@ -1,12 +1,12 @@
 const HeroCarousel = ({images}) => {
 
-    const carouselElem = images.map((image) => {
-        
-    })
+    // const carouselElem = images.map((image) => {
+
+    // })
 
     return (
     <>
-        
+        <h1>Carousel</h1>
     </>
 
     )
