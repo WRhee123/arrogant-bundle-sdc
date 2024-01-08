dce49c95ac9c0a8ce156945632d6d772799932eb
kielBG <143139635+kielBG@users.noreply.github.com>
2024-01-08 10:15:14 -0600

    Footer (#18)
    * footer branch
    
    * updates
    
    * css and layout
    
    * Broke footer compenent down into individual components
    
    * footer language bar coming along
    
    * css and spacing
    
    * editted legal links
    
    * social logos edited
    
    * footer ready for review
    
    * footer.css added
    
    * created a footer components folder

--- src/react/js/components/Footer.jsx
+++ src/react/js/components/Footer.jsx
@@ -1,16 +1,46 @@
+import FooterLanguage from "./FooterContents/FooterLanguage"
+import FooterLegal from "./FooterContents/FooterLegal"
+import FooterLinks from "./FooterContents/FooterLinks"
+import FooterRefer from "./FooterContents/FooterRefer"
+import FooterSocial from "./FooterContents/FooterSocial"
+import FooterTrending from "./FooterContents/FooterTrending"
+import "../../css/footer.css"
+
 const Footer = () => {
 
     return (
     <>
-        <div className='footer ctn'>
-            <div className="footer-contents">
-                <h1>footer</h1>
-                <p>Humble, about, trending games, language selection, socials, copyright, terms of service, legals</p>
-                <p>© 2011-2022 Humble Bundle, Inc. a Ziff Davis company. All Rights Reserved. Humble Bundle and Humble Store are registered trademarks of Humble Bundle, Inc. and may not be used by third parties without express written permission.</p>
+    <div className='footer'>
+        <div className="max-width-wrapper">
+        <div className="footer-content">
+            <div className="brand">
+            <a>
+                <img src="https://cdn.humblebundle.com/static/hashed/4814f84495cd679571cb94896978da3825562075.svg" alt="Humble Bundle Logo" />
+                </a>
+            </div>
+            <FooterRefer />
+            <section className="footer-links">
+            <FooterLinks />
+
+            <FooterTrending />
+            </section>
+            
+            <div className="social-and-language-container">
+
+            <FooterLanguage />
+
+            <FooterSocial />
+            <p className="humble-community-text">The Humble community has contributed over $240,000,000 to charity since 2010,
+                making an amazing difference to causes all over the world.</p>
+            <FooterLegal />
             </div>
         </div>
+        <p className="humble-community-text">&copy; 2011-2022 Humble Bundle, Inc. a Ziff Davis company. All Rights Reserved.
+            Humble Bundle and Humble Store are registered trademarks of Humble Bundle, Inc. and may not be used by third
+            parties without express written permission.</p>
+        </div>
+    </div>
     </>
-
     )
 }
 
