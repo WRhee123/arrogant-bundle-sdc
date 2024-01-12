cec99747491c31db9386f3a4a4ac969d70eac6c6
WRhee123 <rhee.william@yahoo.com>
2024-01-12 14:32:06 -0500

    delete

--- src/react/js/api.js
+++ src/react/js/api.js
@@ -1,5 +1,5 @@
 // This function performs a GET request for a single product
-/*const getProductData = async (id) => {
+const getProductData = async (id) => {
 
     // The url for performing the API request
     const url = `/api/products/${id}`
@@ -16,11 +16,11 @@
     // Otherwise, return the product object (this is the data received)
     return data
 }
-*/
+
 
 import product from "./product-data-hardcode"
-const getProductData = async (id) => {
-    return product
-}
+// const getProductData = async (id) => {
+//     return product
+// }
 
 export default getProductData
